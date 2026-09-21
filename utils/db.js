5a4c6600392a6498baabb2c9e309ca53241f27e3
saitesaraficode10 <newwaypayteam@gmail.com>
2026-09-21 17:26:20 +0400

    Add files via upload

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,11 @@
+const Database = require('better-sqlite3');
+const path = require('path');
+const fs = require('fs');
 
+const dbDir = path.join(__dirname, '..', 'db');
+if (!fs.existsSync(dbDir)) fs.mkdirSync(dbDir, { recursive: true });
+
+const db = new Database(path.join(dbDir, 'amlak.db'));
+db.pragma('journal_mode = WAL');
+db.pragma('foreign_keys = ON');
+module.exports = db;
